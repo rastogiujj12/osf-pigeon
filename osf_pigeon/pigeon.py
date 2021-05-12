d35f27025e382d3a9b7ade321cd5e4867cb4276f
John Tordoff <>
2021-05-12 12:50:25 -0400

    turn off lingering time

--- osf_pigeon/pigeon.py
+++ osf_pigeon/pigeon.py
@@ -37,10 +37,8 @@ import socket
 class KeepAliveClientRequest(ClientRequest):
     async def send(self, conn: "Connection") -> "ClientResponse":
         sock = conn.protocol.transport.get_extra_info("socket")
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 60))
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 60)
-        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 60)
+        sock.setsockopt(socket.IPPROTO_TCP, 0x10, 600)
         return (await super().send(conn))
 
 
